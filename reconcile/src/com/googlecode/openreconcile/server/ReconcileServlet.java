876d01ee3d2b9b039d4d6dac7b19534398eaacef
Tom Morris <tfmorris@gmail.com>
2014-04-08 14:21:23 -0400

    Drop v2.6 of Apache Commons Language jar

--- reconcile/src/com/googlecode/openreconcile/server/ReconcileServlet.java
+++ reconcile/src/com/googlecode/openreconcile/server/ReconcileServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
